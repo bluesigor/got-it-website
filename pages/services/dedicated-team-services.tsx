88b2ddd731bb13ae598ccf586c304a81b6d42a95
Artur <artdrobdev@gmail.com>
2023-02-20 13:38:29 +0200

    photo width update/routes fix

--- pages/services/dedicated-team-services.tsx
+++ pages/services/dedicated-team-services.tsx
@@ -2,15 +2,15 @@ import { TeamServices } from "@/components/services/TeamServices";
 import Head from "next/head";
 import React from "react";
 
-const teamServices = () => {
-  return;
-
-  <>
-    <Head>
-      <title>Got It Agency - Dedicated Team Services for you Business</title>
-    </Head>
-    <TeamServices />
-  </>;
+const TeamServicesPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Got It Agency - Dedicated Team Services for you Business</title>
+      </Head>
+      <TeamServices />
+    </>
+  );
 };
 
-export default teamServices;
+export default TeamServicesPage;
